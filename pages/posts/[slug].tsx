c9c34ebe43c42e0e818cd56f2990ed3afea4704f
Jiru <jiru@jiru-labo.com>
2023-08-14 20:36:22 +0900

    mod

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -44,20 +44,17 @@ const Post = ({ post }) => {
         <ReactMarkdown
           children={post.markdown.parent}
           components={{
-            code({ node, inline, className, children, ...props }) {
+            code({ node, inline, className, children }) {
               const match = /language-(\w+)/.exec(className || "");
               return !inline && match ? (
                 <SyntaxHighlighter
-                  {...props}
                   children={String(children).replace(/\n$/, "")}
                   style={vscDarkPlus}
                   language={match[1]}
                   PreTag="div"
                 />
               ) : (
-                <code {...props} className={className}>
-                  {children}
-                </code>
+                <code>{children}</code>
               );
             },
           }}></ReactMarkdown>
